eab3a45f79e65501e88076db674c3f975d70b944
Vili Ikäheimo <vili.vikaheim@gmail.com>
2023-08-20 12:32:56 +0300

    Update suffle function

--- rust/src/main.rs
+++ rust/src/main.rs
@@ -8,7 +8,7 @@ fn bogo_sort(length: i32, print_iterations_every: i32) {
     print_iteration(&list, iteration);
     while !is_sorted(&list) {
         iteration += 1;
-        shuffle(&mut list, length);
+        shuffle(&mut list);
         if iteration % print_iterations_every == 0 {
             print_iteration(&list, iteration);
         }
@@ -17,10 +17,10 @@ fn bogo_sort(length: i32, print_iterations_every: i32) {
     print_iteration(&list, iteration);
 }
 
-fn shuffle(list: &mut [i32], length: i32) {
-    for n in 0..length {
-        let r: i32 = (rand::random::<i32>() % length).abs();
-        list.swap(n as usize, r as usize)
+fn shuffle(list: &mut [i32]) {
+    for n in 0..list.len() {
+        let r = rand::random::<usize>() % list.len();
+        list.swap(n, r)
     }
 }
 
